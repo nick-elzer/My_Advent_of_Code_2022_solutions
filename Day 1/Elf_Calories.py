49de0a2a6d5030b182059aa83527a2f29bf2863b
nick-elzer <nick@quub.space>
2022-12-02 19:53:41 -0500

    corrected how the data was being called

--- Day 1/Elf_Calories.py
+++ Day 1/Elf_Calories.py
@@ -1,6 +1,8 @@
 #getting data
 with open("day1.in") as file:
-    data = {i for i in file.read().strip().split("/n")}
+    data = [i for i in file.read().strip().split("\n")]
+
+print(data)
 
 max_cals = 0
 elf_cals = 0
